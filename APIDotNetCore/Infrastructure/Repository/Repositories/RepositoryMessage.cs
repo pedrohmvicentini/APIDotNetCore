84435ac3320b8e8b040b6b7d7fd0744623fd9c86
pedrohmvicentini <pedro.medeiros.v@gmail.com>
2024-01-04 16:37:55 -0300

    feat: project starting

---
+++ APIDotNetCore/Infrastructure/Repository/Repositories/RepositoryMessage.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Interfaces;
+using Entities.Entities;
+using Infrastructure.Configuration;
+using Infrastructure.Repository.Generics;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repository.Repositories
+{
+    public class RepositoryMessage : RepositoryGenerics<Message>, IMessage
+    {
+        private readonly DbContextOptions<ContextBase> _OptionsBuilder;
+        public RepositoryMessage()
+        {
+            _OptionsBuilder = new DbContextOptions<ContextBase>();
+        }
+    }
+}
